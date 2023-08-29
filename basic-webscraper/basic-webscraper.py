7c828f13c4348d4133b46e2733dff42fa811909c
Yogita Negi <ynegi6@gmail.com>
2023-08-29 17:47:42 +0100

    title, price, sa scrapable

--- basic-webscraper/basic-webscraper.py
+++ basic-webscraper/basic-webscraper.py
@@ -1,10 +1,23 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
 
 URL = 'https://books.toscrape.com'
-
 req = requests.get(URL)
+# print(req.status_code) to check if it is working
+soup = BeautifulSoup(req.content, 'html.parser')
 
-# soup = BeautifulSoup(URL, 'html.parser')
-# print(soup)
+articles = soup.find_all('article', class_='product_pod')
+
+# print(articles)
+for book in articles:
+    print(book)
+    title = book.find('h3').text
+    print(title)
+    price = book.find('p',class_='price_color').text
+    print(price)
+    stock_availability = book.find('p',class_='instock availability').text
+    print(stock_availability)
+
