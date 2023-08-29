d36ad49539705f06300f1da530ab3d1bfae2f07c
Yogita Negi <ynegi6@gmail.com>
2023-08-29 22:09:01 +0100

    defined function add data to df and then to excel csv

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Learning about Python, webscraping and API's
 Test website : https://books.toscrape.com
 
 HTTP Client --> request ---> to get document behind a URL ---> SOUP
+
+## webscraping-tables
+read data into Dataframe
+
+df = pd.DataFrame(data)
+
+read data toe xcel or csv
+
+df.to_csv('leaguetable.csv')
+df.to_excel('leaguetable.xlsx)
--- webscraping-tables/webscraping-tables.py
+++ webscraping-tables/webscraping-tables.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+def get_league_table(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    tablelist = []
+    league_table = soup.find('table', class_='standing-table__table callfn').find('tbody')
+    for team in league_table.find_all('tr'):
+        pl_team = team.find('td', class_='standing-table__cell standing-table__cell--name').text.strip()
+        pl_points = team.find_all('td', class_='standing-table__cell')[9].text
+        teaminleague = {
+            'name': pl_team,
+            'points': pl_points
+        }
+        tablelist.append(teaminleague)
+    return tablelist
 
-URL = 'https://www.skysports.com/premier-league-table'
 
-req = requests.get(URL)
+data = get_league_table('https://www.skysports.com/premier-league-table')
+# print(data)
 
-# print(req.status_code)
+df = pd.DataFrame(data) # read data into dataframe
+print(df.head())
+df.to_csv('leaguetable.csv')
+df.to_excel('leaguetable.xlsx')
+print('save to file')
 
-soup = BeautifulSoup(req.content, 'html.parser')
-
-league_table = soup.find('table',class_='standing-table__table callfn').find('tbody')
-
-# print(league_table)
-
-for row in league_table.find_all('tr'):
-    pl_team = row.find('td', class_='standing-table__cell standing-table__cell--name').text.strip()
-    # print(pl_team)
-    pl_points = row.find_all('td', class_='standing-table__cell')[9].text
-    print(pl_team, pl_points)
 
 
 
