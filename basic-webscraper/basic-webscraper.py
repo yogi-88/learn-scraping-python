1b4472be553fb10fe2e49443e05ba6e208149b71
Yogita Negi <ynegi6@gmail.com>
2023-08-29 18:22:10 +0100

    all details of books added to list

--- basic-webscraper/basic-webscraper.py
+++ basic-webscraper/basic-webscraper.py
@@ -11,13 +11,24 @@ soup = BeautifulSoup(req.content, 'html.parser')
 
 articles = soup.find_all('article', class_='product_pod')
 
+book_list = []
 # print(articles)
 for book in articles:
-    print(book)
+    # print(book)
     title = book.find('h3').text
-    print(title)
+    # print(title)
     price = book.find('p',class_='price_color').text
-    print(price)
-    stock_availability = book.find('p',class_='instock availability').text
-    print(stock_availability)
+    # print(price)
+    instock = book.find('p',class_='instock availability').text.strip()
+    # print(instock)
+    books = {
+        'Title': title,
+        'Price': price,
+        'InStock': instock
+    }
+    book_list.append(books)
 
+
+
+
+print(book_list)
