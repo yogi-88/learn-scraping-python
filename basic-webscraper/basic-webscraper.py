be2262454b7f2a43b073d618d072052a3a70586f
Yogita Negi <ynegi6@gmail.com>
2023-08-29 18:41:00 +0100

    pagination is done, scraping data for all pages

--- basic-webscraper/basic-webscraper.py
+++ basic-webscraper/basic-webscraper.py
@@ -1,34 +1,36 @@
-import re
 
+import sys
 import requests
 from bs4 import BeautifulSoup
-
-
-URL = 'https://books.toscrape.com'
-req = requests.get(URL)
-# print(req.status_code) to check if it is working
-soup = BeautifulSoup(req.content, 'html.parser')
-
-articles = soup.find_all('article', class_='product_pod')
-
 book_list = []
-# print(articles)
-for book in articles:
-    # print(book)
-    title = book.find('h3').text
-    # print(title)
-    price = book.find('p',class_='price_color').text
-    # print(price)
-    instock = book.find('p',class_='instock availability').text.strip()
-    # print(instock)
-    books = {
-        'Title': title,
-        'Price': price,
-        'InStock': instock
-    }
-    book_list.append(books)
+
+for i in range(1,51):
+    print(i)
+    URL = f'https://books.toscrape.com/catalogue/page-{i}.html'
+    req = requests.get(URL)
+    # print(req.status_code) to check if it is working
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    articles = soup.find_all('article', class_='product_pod')
+
+
+    # print(articles)
+    for book in articles:
+        # print(book)
+        title = book.find_all('a')[1]['title']
+        # print(title)
+        price = book.find('p', class_='price_color').text
+        # print(price)
+        instock = book.find('p', class_='instock availability').text.strip()
+        # print(instock)
+        books = {
+            'Title': title,
+            'Price': price,
+            'InStock': instock
+        }
+        book_list.append(books)
+
+print(len(book_list))
 
 
 
-
-print(book_list)
